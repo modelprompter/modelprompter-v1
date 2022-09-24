5496218aa9f4f8e2318659c9210bdf89489d4327
Everest Wonder <mounteverestwonder@gmail.com>
2022-09-24 15:59:17 -0700

    #11 Expand data feed to show more content

--- src/components/DataFeed.vue
+++ src/components/DataFeed.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
-q-drawer(v-model='isRightSidebarOpened' bordered side='right')
-  q-list
-    q-item-label(header) Data feed
-  .q-pa-md
-    q-card.q-mb-md(v-for='(item, i) in dataFeed.data' :data='item')
-      q-img.clickable(v-if='item.image' :src='item.image' @click='expand(item)')
-      q-card-section(v-else)
-        pre {{item.data}}
+div(:class='{"is-drawer-maximized": isExpanded}')
+  q-drawer(v-model='isRightSidebarOpened' bordered side='right')
+    q-list
+      q-item-label(header)
+        q-btn.bg-dark.text-white(v-if='isExpanded' icon='east' @click='isExpanded = false')
+          span.q-ml-sm Collapse Data Feed
+        q-btn.bg-dark.text-white(v-else icon='west' @click='isExpanded = true')
+          span.q-ml-sm Expand Data Feed
+    .q-pa-md
+      .row.q-col-gutter-md.items-start
+        q-card.q-mb-md(v-for='(item, i) in dataFeed.data' :data='item' :class='[isExpanded ? "col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-1" : "col-6"]')
+          q-img.clickable(v-if='item.image' :src='item.image' @click='expand(item)')
+          q-card-section(v-else)
+            pre {{item.data}}
 
-  //- Image Modal
-  q-dialog(v-model='imageModal')
-    q-card(style='min-width: 300px')
-      q-img(native-context-menu :src='imageModalActiveImage.image' :style='{width: imageModalActiveImage.width, height: imageModalActiveImage.height}')
-      q-card-section
-        pre(style='font-size:1.15em') {{imageModalActiveImage}}
-      q-card-actions(align='right')
+    //- Image Modal
+    q-dialog(v-model='imageModal')
+      q-card(style='min-width: 300px')
+        q-img(native-context-menu :src='imageModalActiveImage.image' :style='{width: imageModalActiveImage.width, height: imageModalActiveImage.height}')
+        q-card-section
+          pre(style='font-size:1.15em') {{imageModalActiveImage}}
+        q-card-actions(align='right')
           q-btn(flat round color='negative' icon='delete' @click='deleteImage($event, imageModalActiveImage)')
           q-space
           q-btn(flat round color='secondary' icon='save' @click='downloadImage($event, imageModalActiveImage)')
@@ -27,6 +33,7 @@ const props = defineProps(['data', 'isRightSidebarOpened'])
 const dataFeed = useDatafeedResponses()
 const imageModal = $ref(false)
 const imageModalActiveImage = $ref({})
+const isExpanded = $ref(false)
 
 function expand (data) {
   imageModal = true
