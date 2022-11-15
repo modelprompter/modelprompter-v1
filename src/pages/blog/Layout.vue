5928cfdb456fb2e28164b1430200bcb00e3416f0
Oz Ramos <heyozramos@gmail.com>
2022-11-14 23:51:15 -0800

    #0 Minor improvements to sidebar

--- src/pages/blog/Layout.vue
+++ src/pages/blog/Layout.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 q-page
-  iframe(src='https://blog.modelprompter.com' style='min-height: inherit; width: 100%; height: 100%; border: none;')
+  .content(style='position: absolute; top: 0; bottom: 0; left: 0; right: 0;')
+    iframe(src='https://blog.modelprompter.com' style='min-height: inherit; width: 100%; height: 100%; border: none;')
 </template>
 
 <script setup>
